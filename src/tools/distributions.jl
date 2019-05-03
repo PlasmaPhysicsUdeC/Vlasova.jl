c41eaa38af7e9f6c88b7ad00ca9c8a4b088334e1
Jorge Gidi <jorgegidi@gmail.com>
2019-05-03 03:20:01 -0400

    added distributions

--- src/tools/distributions.jl
+++ src/tools/distributions.jl
@@ -1,37 +1,47 @@
+export maxwellian1d, maxwellian2d, twostream1d, bump_on_tail1d
 
 # 1D normalized Maxwellian
-function maxwellian1d(velocity::Array{Float64, 1}; thermalVelocity::Float64 = 1.0, driftVelocity = 0.0::Float64) # Args as keywords
+function maxwellian1d(v; vt::Float64 = 1.0, vd = 0.0::Float64) # Velocity may be anything (Float64, Array{Float64}, etc...)
     #=
     Returns a normalized 1-dimensional Maxwellian.
-    If not provided: thermalVelocity = 1.0, driftVelocity = 0.0
+    If not provided: vt = 1.0, vd = 0.0
     =#
 
-    normalizationConstant = 1.0/(thermalVelocity*sqrt(2pi)) # Normalization constant
-    return @. normalizationConstant * exp( -0.5*( (velocity-driftVelocity)/thermalVelocity )^2 )
+    c = 1.0/(vt*sqrt(2pi)) # Normalization constant
+    return @. c * exp( -0.5*( (v - vd)/vt )^2 )
 end
 
+function maxwellian2d(vx, vy; vtx::Float64 = 1.0, vty::Float64 = 1.0, vdx::Float64 = 0.0, vdy::Float64 = 0.0)
+    #=
+    Returns a normalized 2-dimensional Maxwellian.
+    If not provided: Vtx, Vty = 1.0 and v0x, v0y = 0.0
+    =#
+    Mx = maxwellian1d(vx, vt = vtx, vd = vdx)
+    My = maxwellian1d(vy, vt = vty, vd = vdy)
+    
+    return Mx*My'
+end
 
 # 1D two stream instability, monospecies
-function twoStream1sp1d(velocity::Array{Float64,1}; thermalVelocity1::Float64 = 1.0, thermalVelocity2::Float64 = 1.0, driftVelocity1::Float64 = -2.0, driftVelocity2::Float64 = 2.0, nb1::Float64 = 0.5, nb2::Float64 = 0.5)
+function twostream1d(v; vt1::Float64 = 1.0, vt2::Float64 = 1.0, vd1::Float64 = -2.0, vd2::Float64 = 2.0, n1::Float64 = 0.5, n2::Float64 = 0.5)
     #=
     Returns a normalized 1-dimensional two stream distribution.
-    If not provided: thermalVelocity1 = thermalVelocity = 1.0, driftVelocity1 = driftVelocity2 = -2.0 
     =#
 
-    normalizationConstant1 = nb1/(thermalVelocity1*sqrt(2pi)) # Normalization constant
-    normalizationConstant2 = nb2/(thermalVelocity2*sqrt(2pi)) # Normalization constant
-    return @. normalizationConstant1 * exp( -0.5*(velocity - driftVelocity1)^2/thermalVelocity1^2 ) + normalizationConstant2 * exp( -0.5*(velocity - driftVelocity2)^2/thermalVelocity2^2 )
+    # Normalization constants
+    c1 = n1 / ( vt1*sqrt(2pi) ) 
+    c2 = n2 / ( vt2*sqrt(2pi) )
+    return @. c1 * exp( -0.5*( (v - vd1)/vt1 )^2 ) + c2 * exp( -0.5*( (v - vd2)/vt2 )^2 )
 end
 
 
 # 1D bump on tail distribution, monospecies
-function bumpOnTail1d(velocity::Array{Float64,1}; thermalVelocityBeam::Float64 = 0.5, driftVelocityBeam::Float64 = 4.5, densityCore::Float64 = 0.9, densityBeam::Float64 = 0.1)
+function bump_on_tail1d(v; vtb::Float64 = 0.5, vdb::Float64 = 4.5, nc::Float64 = 0.9, nb::Float64 = 0.1)
     #=
-    Returns a normalized 1-dimensional two stream distribution.
-    If not provided: thermalVelocity1 = thermalVelocity = 1.0, driftVelocity1 = driftVelocity2 = -4.0 
+    Returns a normalized 1-dimensional bump on tail distribution.
     =#
-
-    normalizationConstant1 = densityCore/sqrt(2pi) # Normalization constant
-    normalizationConstant2 = densityBeam/(thermalVelocityBeam*sqrt(2pi)) # Normalization constant
-    return @. normalizationConstant1 * exp( -0.5*( velocity )^2) + normalizationConstant2 * exp( -0.5*(velocity - driftVelocityBeam)^2/thermalVelocityBeam^2)
+    
+    c1 = nc / sqrt(2pi) # Normalization constant
+    c1 = nb / (vtb * sqrt(2pi)) # Normalization constant
+    return @. c1 * exp( -0.5*v^2 ) + c2 * exp( -0.5 * ((v - vdb)/vtb)^2 )
 end
