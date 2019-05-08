577feb3c178faaf628974bc1bddb36659b410b91
Jorge Gidi <jorgegidi@gmail.com>
2019-05-08 12:01:28 -0400

    fix on build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,15 +1,21 @@
 #TODO: Use BinDeps
 
 # Compile fortran libraries to deps/src/usr/lib
-function compile_fortran(filename::String)
-    current_path = dirname(@__FILE__)
-    prefix = "$current_path/src/"
-    postfix = "$current_path/usr/lib/"
+function compile_fortran(filename::String, prefix::String, postfix::String)
     run(`gfortran -Wall -shared -fPIC -O3 -fopenmp $prefix$filename.f90 -o $postfix$filename.so`)
 end
 
 # All files to be compiled
 source_code = ["advections", "velocity_filter"]
+
+# Paths
+current_path = dirname(@__FILE__)
+prefix = "$current_path/src/"
+postfix = "$current_path/usr/lib/"
+
+# Compile
+mkpath(prefix)
+mkpath(postfix)
 for file in source_code
-    compile_fortran(file)
+    compile_fortran(file, prefix, postfix)
 end
