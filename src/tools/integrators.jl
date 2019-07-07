3e0175fba5141c35eec9ef413604132c9c6a1c1c
Jorge Gidi <jorgegidi@gmail.com>
2019-07-07 00:59:04 -0400

    add integrator ABABABA

--- src/tools/integrators.jl
+++ src/tools/integrators.jl
@@ -1,4 +1,4 @@
-export verlet_position, verlet_velocity, mclachlan_velocity, BABAB
+export verlet_position, verlet_velocity, mclachlan_velocity, BABAB, ABABABA
 
 verlet_position = VlasovaIntegrator("ABA", [0.5,
                                             1.0,
@@ -17,5 +17,12 @@ BABAB = VlasovaIntegrator("BABAB", [0.19318332750378361,
                                     0.6136333449924327,
                                     0.5,
                                     0.19318332750378361 ])
+
+begin
+    lambda = 1/(2 - 2^(1/3))
+    teta = lambda / 2
+
+    global ABABABA = VlasovaIntegrator("ABABABA", [teta, lambda, 0.5 - teta, 1 - 2*lambda , 0.5 - teta, lambda, teta])
+end
 
 # TODO: Implement the **120 Eff integrator from Omelyan2003
