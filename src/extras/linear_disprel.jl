fd8ce089667a39a46a4f9d06a969daa4cba3a6ef
Jorge Gidi <jorgegidi@gmail.com>
2019-07-19 14:27:31 -0400

    TODO on linear disprel

---
+++ src/extras/linear_disprel.jl
@@ -0,0 +1,16 @@
+#=
+
+TODO: Make function that, given the initial distribution can find omega from k using the secant method.
+
+=#
+
+function find_linear_frequency(f0::Array{Float64}; wavenumber)
+    omega = zero(Complex{Float64})
+
+   return omega 
+end
+
+
+function dielectric_tensor(f0, omega, k)
+    
+end
