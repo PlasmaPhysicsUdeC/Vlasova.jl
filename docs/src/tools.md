fa6d5a2de63204d3cc2bb27eec713ea3840d6c89
Jorge Gidi <jorgegidi@gmail.com>
2019-09-21 19:53:01 -0300

    docs

--- docs/src/tools.md
+++ docs/src/tools.md
@@ -7,6 +7,10 @@ Public = true
 ```
 
 ## Symplectic Integrators
+
+# TODO: Cite verlet integrators
+Vlasova implements a way to define arbitrary gradient and non-gradient symplectic integrators with the function [`VlasovaIntegrator`](@ref). However, a few integrators come defined by default. Those integrators are the following, and unless explicitly cited, they were taken from [`Omelyan (2003)`](https://www.sciencedirect.com/science/article/pii/S0010465502007543).
+
 ```@autodocs
 Modules = [Vlasova]
 Pages = ["integrators.jl"]
--- src/tools/integrators.jl
+++ src/tools/integrators.jl
@@ -1,27 +1,53 @@
 export verlet_position, verlet_velocity, mclachlan_velocity, BABAB, ChinA, ChinC, ABABABA, ABACABA
 
 """
-Verlet scheme starting with position.
+```julia
+VlasovaIntegrator("ABA", [0.5,
+                          1.0,
+                          0.5])
+```
+
+Developed by [`Verlet (1967)`](https://journals.aps.org/pr/abstract/10.1103/PhysRev.159.98).
 """
 verlet_position = VlasovaIntegrator("ABA", [0.5,
                                             1.0,
                                             0.5])
 
 """
-Verlet scheme starting with velocity.
+```julia
+VlasovaIntegrator("BAB", [0.5,
+                          1.0,
+                          0.5])
+```
+
+Developed by [`Swope et al. (1982)`](https://aip.scitation.org/doi/10.1063/1.442716).
 """
 verlet_velocity = VlasovaIntegrator("BAB", [0.5,
                                             1.0,
                                             0.5])
 
 """
-Doc this! [`McLachlan et al. (1992)`](https://iopscience.iop.org/article/10.1088/0951-7715/5/2/011)
+```julia
+VlasovaIntegrator("BABA", [1 - 1/sqrt(2),
+                           1/sqrt(2),
+                           1/sqrt(2),
+                           1 - 1/sqrt(2)])
+```
+
+Developed by [`McLachlan et al. (1992)`](https://iopscience.iop.org/article/10.1088/0951-7715/5/2/011).
 """
 mclachlan_velocity = VlasovaIntegrator("BABA", [1 - 1/sqrt(2),
                                                 1/sqrt(2),
                                                 1/sqrt(2),
                                                 1 - 1/sqrt(2)])
 """
+```julia
+VlasovaIntegrator("BABAB", [0.19318332750378361,
+                            0.5,
+                            0.6136333449924327,
+                            0.5,
+                            0.19318332750378361 ])
+```
 """
 BABAB = VlasovaIntegrator("BABAB", [0.19318332750378361,
                                     0.5,
@@ -29,6 +55,18 @@ BABAB = VlasovaIntegrator("BABAB", [0.19318332750378361,
                                     0.5,
                                     0.19318332750378361 ])
 
+"""
+```julia
+VlasovaIntegrator("BACAB", [ 1/6,
+                             1/2,
+                             2/3,
+                             1/2,
+                             1/6 ],
+                          gradient_coefficients = [ 1/48 ] )
+```
+
+Developed by [`Chin (1997)`](https://www.sciencedirect.com/science/article/abs/pii/S03759601970000308).
+"""
 ChinA = VlasovaIntegrator("BACAB", [1/6,
                                     1/2,
                                     2/3,
@@ -36,6 +74,20 @@ ChinA = VlasovaIntegrator("BACAB", [1/6,
                                     1/6],
                           gradient_coefficients = [ 1/48 ] )
 
+"""
+```julia
+VlasovaIntegrator("ABACABA", [ 1/6,
+                               3/8,
+                               1/3,
+                               1/4,
+                               1/3,
+                               3/8,
+                               1/6 ],
+                            gradient_coefficients = [ 1 / 48 ] )
+```
+
+Developed by [`Chin (1997)`](https://www.sciencedirect.com/science/article/abs/pii/S03759601970000308).
+"""
 ChinC = VlasovaIntegrator("ABACABA", [1/6,
                                       3/8,
                                       1/3,
@@ -46,32 +98,68 @@ ChinC = VlasovaIntegrator("ABACABA", [1/6,
                           gradient_coefficients = [ 1 / 48 ])
 
 let
-    lambda = 1/(2 - 2^(1/3))
-    teta = lambda / 2
+    λ = 1/(2 - 2^(1/3))
+    θ = λ / 2
 
-    global ABABABA = VlasovaIntegrator("ABABABA", [teta,
-                                                   lambda,
-                                                   0.5 - teta,
-                                                   1 - 2*lambda,
-                                                   0.5 - teta,
-                                                   lambda,
-                                                   teta])
+    """
+    ```julia
+    VlasovaIntegrator("ABABABA", [ θ,
+                                   λ,
+                                   0.5 - θ,
+                                   1 - 2λ,
+                                   0.5 - θ,
+                                   λ,
+                                   θ])
+    ```
+    where
+
+    ```julia
+    λ = 1/(2 - 2^(1/3))
+    θ = λ / 2
+    ```
+    """
+    global ABABABA = VlasovaIntegrator("ABABABA", [θ,
+                                                   λ,
+                                                   0.5 - θ,
+                                                   1 - 2λ,
+                                                   0.5 - θ,
+                                                   λ,
+                                                   θ ])
 end
 
 
 let
-    xi = 0.006938106540706989
-    lambda = 0.2470939580390842
-    teta = 0.08935804763220157
+    ξ = 0.006938106540706989
+    λ = 0.2470939580390842
+    θ = 0.08935804763220157
 
-    global ABACABA = VlasovaIntegrator("ABACABA", [teta,
-                                                   lambda,
-                                                   0.5 - teta,
-                                                   1 - 2*lambda,
-                                                   0.5 - teta,
-                                                   lambda,
-                                                   teta],
-                                       gradient_coefficients = [xi])
+    """
+    ```julia
+    VlasovaIntegrator("ABACABA", [ θ,
+                                   λ,
+                                   0.5 - θ,
+                                   1 - 2λ,
+                                   0.5 - θ,
+                                   λ,
+                                   θ ],
+                                 gradient_coefficients = [ ξ ])
+    ```
+    where
+
+    ```julia
+    ξ = 0.006938106540706989
+    λ = 0.2470939580390842
+    θ = 0.08935804763220157
+    ```
+    """
+    global ABACABA = VlasovaIntegrator("ABACABA", [ θ,
+                                                    λ,
+                                                    0.5 - θ,
+                                                    1 - 2λ,
+                                                    0.5 - θ,
+                                                    λ,
+                                                    θ ],
+                                                 gradient_coefficients = [ ξ ])
     # TODO: Check gradient coef
 end
 
